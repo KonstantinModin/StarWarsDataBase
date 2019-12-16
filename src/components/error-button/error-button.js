085111dbddcb8bc123edf2cd5cde1cdacd909ab8
Konstantin <konstantin.v.modin@gmail.com>
2019-12-16 06:34:46 +0100

    updates

--- src/components/error-button/error-button.js
+++ src/components/error-button/error-button.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './error-button.css';
+import "./error-button.css";
 
 export default class ErrorButton extends Component {
+    state = {
+        renderError: false
+    };
 
-  state = {
-    renderError: false
-  };
+    render() {
+        if (this.state.renderError) { this.foo.bar = 0 };
 
-  render() {
-    if (this.state.renderError) {
-      this.foo.bar = 0;
-    }
-
-    return (
-      <button
-        className="error-button btn btn-danger btn-lg"
-        onClick={() => this.setState({renderError: true})}>
-        Throw Error
-      </button>
-    );
-  }
+        return (
+            <button
+                className="error-button btn btn-danger btn-lg"
+                onClick={() => this.setState({ renderError: true })}
+            >
+                Throw Error
+            </button>
+        )
+    };
 }
--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -6,12 +6,10 @@ import icon from './death-star.png';
 const ErrorIndicator = () => {
     return (
         <div className="error-indicator jumbotron rounded">
-            <img src={icon} alt="error icon"/>
-            
+            <img src={icon} alt="error icon"/>            
                 <span className="boom">BOOM!</span>
                 <span>something has gone terribly wrong</span>
-                <span>(but we already sent droids to fix it)</span>           
-            
+                <span>(but we already sent droids to fix it)</span>    
         </div>
     );
 };
