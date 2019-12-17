e53651fb1c34abef7c4e9af38086c3e73c9aa054
Konstantin <konstantin.v.modin@gmail.com>
2019-12-17 13:05:51 +0100

    updates

--- src/components/pages/secret-page.js
+++ src/components/pages/secret-page.js
@@ -1,15 +1,11 @@
 import React from 'react';
-// import { Redirect } from 'react-router-dom';
 
-const SecretPage = ({ isloggedIn }) => {
-    // if (isloggedIn) {
-        return (
-            <div className="jumbotron text-center">
-                <h3>This is the secret page!</h3>
-            </div>
-        );
-    // }
-    // return <Redirect to="/login" />;
+const SecretPage = ({ isloggedIn }) => {    
+    return (
+        <div className="jumbotron text-center">
+            <h3>This is the secret page!</h3>
+        </div>
+    );    
 };
 
 export default SecretPage;
