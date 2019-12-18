2ec28173b2e0f4e8f64e881033cde83257398893
Konstantin <konstantin.v.modin@gmail.com>
2019-12-18 06:45:15 +0100

    updates

---
+++ src/components/error-boundry/error-boundary.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import ErrorIndicator from '../error-indicator';
+
+class ErrorBoundary extends Component {
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch = () => { // change to getDerivedStateFromError        
+        this.setState({
+            hasError: true
+        });  
+    };
+    
+    render() {
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        return (
+            this.props.children
+        )
+    }
+}
+
+export default ErrorBoundary;
